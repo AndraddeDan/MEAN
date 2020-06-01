8b7e51c7800e378029b636a13da810bef5223a56
Daniel Andrade <andraddedan@outlook.com>
2020-06-01 15:08:16 -0300

    front improvement

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plano } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public selectedDestino: string;
+  public selectedOrigem: string;
+  public selectedPlano: string;
+
+  public comFaleMaisValue: string;
+  public semFaleMaisValue: string;
+
+  public ddd: number[] = [11, 16, 17, 18];
+  public planos: Plano[] = [];
+
+  ngOnInit() {
+
+  }
 }
