bc15a0f8d925c86eee9db4325e186b0162719a45
Daniel Andrade <andraddedan@outlook.com>
2020-06-01 18:07:32 -0300

    database data

---
+++ database/planos.js
@@ -0,0 +1,15 @@
+db.plano.drop();
+db.plano.insert([
+  {
+    "planoNome": "FaleMais 30",
+    "minutosGratis": 30
+  },
+  {
+    "planoNome": "FaleMais 60",
+    "minutosGratis": 60
+  },
+  {
+    "planoNome": "FaleMais 120",
+    "minutosGratis": 120
+  },
+]);
---
+++ database/precos.js
@@ -0,0 +1,33 @@
+db.preco.drop();
+db.preco.insert([
+  {
+    "origem" : "011",
+    "destino" : "016",
+    "valorMinuto" : "1.90"
+  },
+  {
+    "origem" : "016",
+    "destino" : "011",
+    "valorMinuto" : "2.90"
+  },
+  {
+    "origem" : "011",
+    "destino" : "017",
+    "valorMinuto" : "1.70"
+  },
+  {
+    "origem" : "017",
+    "destino" : "011",
+    "valorMinuto" : "2.70"
+  },
+  {
+    "origem" : "011",
+    "destino" : "018",
+    "valorMinuto" : "0.90"
+  },
+  {
+    "origem" : "018",
+    "destino" : "011",
+    "valorMinuto" : "1.90"
+  },
+]);
